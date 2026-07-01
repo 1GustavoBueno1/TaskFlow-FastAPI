d03049134fb60e9f2de10a29ce82974189b094fc
gustavo <gustavo.silva123913@gmail.com>
2026-07-01 06:27:21 -0300

    Atualiza arquivos binários e configurações do Alembic

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ from app.models.routes.task_routes import rotas_tarefas
 from app.models.routes.user_routes import rotas_user
 app = FastAPI()
 Base.metadata.create_all(bind = engine)
-
-
 app.include_router(rotas_tarefas)
 app.include_router(rotas_user)
