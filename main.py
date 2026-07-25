f48f302db1c1214d2af79c361851c30eb46a2fc0
gustavo <gustavo.silva123913@gmail.com>
2026-07-24 21:33:01 -0300

    Adiciona middleware CORS para permitir requisições de origens específicas

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from app.models.models.database import engine, Base
 from app.models.routes.task_routes import rotas_tarefas
 from app.models.routes.user_routes import rotas_user
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(rotas_tarefas)
 app.include_router(rotas_user)
